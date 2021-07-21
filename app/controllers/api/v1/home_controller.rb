353b8b43f7e74ee11c7c5fc9af3d44a6c11dac36
Ajay Arsud <ajay.arsud@gmail.com>
2021-07-21 18:51:43 +0530

    Corrected routes and removed unwanted code.

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -2,24 +2,19 @@ class Api::V1::HomeController < ApplicationController
   def create
     DataParserService.run(params).match do
       success do |user|
-        # flash[:success] = "Data from CSV loaded successfully!"
-        # redirect_to root_path
         render success: { success: "Data from CSV loaded successfully!"}, status: 201
       end
 
       failure(:invalid_data) do |error_data|
-        # render_form_errors("Oops, invalid csv. fix your mistakes and try again")
         render error: { error: "Data from CSV loaded successfully!"}, status: 400
       end
 
       failure(:dataset_save_failed) do |error_data|
-        # render_form_errors("Sorry, something went wrong on our side.")
         render error: { error: "Data from CSV loaded successfully!"}, status: 400
       end
 
       failure do |exception|
         render error: { error: "Data from CSV loaded successfully!"}, status: 400
-        # render_form_errors("something went terribly wrong")
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index'
-
   # resources :home, only: %i(index create)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       resources :questions
